b688ae71f8e8f371e42199026af346576f873a53
JonGudenzi <jongudenzi@gmail.com>
2022-04-12 22:17:08 -0400

    initial setup

---
+++ multi-store-raffle/src/App.js
@@ -0,0 +1,12 @@
+import CustomerInfo from "./components/CustomerInfo";
+
+
+function App() {
+  return (
+    <div className="App">
+      <CustomerInfo />
+    </div>
+  );
+}
+
+export default App;
---
+++ multi-store-raffle/src/components/CustomerInfo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CustomerInfo = () => {
+return (
+    <h1>Hello</h1>
+)
+};
+
+export default CustomerInfo;
